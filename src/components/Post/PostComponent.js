73d8854fa88f21f49907fe8caab1df131d221a63
Juan Ignacio <juan@Juans-MacBook-Pro.local>
2020-07-17 00:02:09 -0400

    Metodo eliminar Post

--- src/components/Post/PostComponent.js
+++ src/components/Post/PostComponent.js
@@ -9,129 +9,172 @@ import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import Button from '@material-ui/core/Button';
 import DeleteIcon from '@material-ui/icons/Delete';
-import Icon from '@material-ui/core/Icon';
 import Tooltip from '@material-ui/core/Tooltip';
 import TextField from '@material-ui/core/TextField';
 import SaveIcon from '@material-ui/icons/Save';
-
+import axios from 'axios';
 import './PostComponent.css';
 
 
-function createData(name, description) {
-    return { name, description };
-}
 
 
 let rows = [];
+
+let urlApi = "http://localhost:3700"
 
 
 
 class PostComponent extends React.Component {
 
+
     nameRef = React.createRef();
     descriptionRef = React.createRef();
 
-    constructor(props) {
-        rows = [createData('post1', "159"),
-        createData('post2', "237"),
-        createData('post3', "262"),
-        createData('post4', "305"),
-        createData('post5', "356"),]
-        super(props);
-        this.state = {
-            rowFilter: rows
-        };
 
+    state = {
+        rowFilter: [],
+        loading: true,
+        open: true,
+    };
+
+    getPost() {
+        axios.get(`${urlApi}/post`).then(res => {
+            rows = res.data.message;
+            this.setState({
+                rowFilter: rows,
+                loading: false
+            });
+        });
     }
+  
+constructor(props){
+    super(props);
+    this.getPost();
+}
+
 
     remove = (index, name) => {
         rows.splice(index, 1);
         this.setState({
             rowFilter: rows
         });
+        console.log("nombre a eliminar")
+        axios.delete(`${urlApi}/post`, {data:{ name: name }})
+            .then(function (response) {
+                console.log(response.data);
+    })
+     
+}
+
+
+searchPostByName = (event) => {
+
+    if (!event.target.value) {
+        this.setState({
+            rowFilter: rows
+        })
+    } else {
+        this.setState({
+            rowFilter: rows.filter((row) =>
+                row.name.toLowerCase()
+                    .normalize('NFD')
+                    .replace(/[\u0300-\u036f]/g, '')
+                    .includes(event.target.value.toLowerCase())
+            )
+        })
     }
+}
 
+submitPost = (e) => {
+    e.preventDefault();
 
-    searchPostByName = (event) => {
-
-        if (!event.target.value) {
+    var newPost = { name: this.nameRef.current.value, description: this.descriptionRef.current.value };
+    axios.post(`${urlApi}/post`, newPost)
+        .then(function (response) {
+            console.log("respuesta", response.data);
+            rows.push(response.data);
+            console.log("rows",rows);
             this.setState({
                 rowFilter: rows
-            })
-        } else {
-            this.setState({
-                rowFilter: rows.filter((row) =>
-                    row.name.toLowerCase()
-                        .normalize('NFD')
-                        .replace(/[\u0300-\u036f]/g, '')
-                        .includes(event.target.value.toLowerCase())
-                )
-            })
-        }
-    }
+            });
+            return (
+                <h2>Creado</h2>
+            )
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
 
-    submitPost = (e) => {
-        e.preventDefault();
-        console.log(this.nameRef.current.value);
-    };
 
-    render() {
+};
 
+render() {
+
+    if (!this.state.loading) {
         return (
-            <Container>
-                <label className="search-input">Buscar por nombre</label>
-                <input className="search-input" type="text" name="searchName" onKeyUp={this.searchPostByName} />
-                <TableContainer component={Paper}>
-                    <Table size="small" aria-label="a dense table">
-                        <TableHead>
-                            <TableRow>
-                                <TableCell >Nombre</TableCell>
-                                <TableCell align="center">Descripción</TableCell>
-                                <TableCell align="right"></TableCell>
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {this.state.rowFilter.map((row, index) =>
-                                <TableRow key={row.name}>
-                                    <TableCell component="th" scope="row">
-                                        {row.name}
-                                    </TableCell>
-                                    <TableCell align="center">{row.description}</TableCell>
-                                    <TableCell align="right">
-                                        <Tooltip title="Eliminar">
-                                            <Button onClick={() => this.remove(index, row.name)} startIcon={<DeleteIcon />}>
-                                            </Button>
-                                        </Tooltip>
-                                    </TableCell>
+
+            <div>
+                <Container>
+                    <label className="search-input">Buscar por nombre</label>
+                    <input className="search-input" type="text" name="searchName" onKeyUp={this.searchPostByName} />
+                    <TableContainer component={Paper}>
+                        <Table size="small" aria-label="a dense table">
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell >Nombre</TableCell>
+                                    <TableCell align="center">Descripción</TableCell>
+                                    <TableCell align="right"></TableCell>
                                 </TableRow>
-                            )
+                            </TableHead>
+                            <TableBody>
 
-                            }
-                        </TableBody>
-                    </Table>
-                </TableContainer>
+                                {this.state.rowFilter.map((row, index) =>
+                                    <TableRow key={row.name}>
+                                        <TableCell component="th" scope="row">
+                                            {row.name}
+                                        </TableCell>
+                                        <TableCell align="center">{row.description}</TableCell>
+                                        <TableCell align="right">
+                                            <Tooltip title="Eliminar">
+                                                <Button onClick={() => this.remove(index, row.name)} startIcon={<DeleteIcon />}>
+                                                </Button>
+                                            </Tooltip>
+                                        </TableCell>
+                                    </TableRow>
+                                )
 
-                <form className="form" onSubmit={this.submitPost}>
-                    <div className="form-group">
-                        <TextField required name="name" label="Nombre" inputRef={this.nameRef} />
-                    </div>
-                    <div className="form-group">
-                        <TextField required inputRef={this.descriptionRef}
-                            name="description"
-                            label="Descripción"
-                            multiline
+                                }
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
 
-                        />
 
-                    </div>
+                    <form className="form" onSubmit={this.submitPost}>
+                        <div className="form-group">
+                            <TextField required name="name" label="Nombre" inputRef={this.nameRef} />
+                        </div>
+                        <div className="form-group">
+                            <TextField required inputRef={this.descriptionRef}
+                                name="description"
+                                label="Descripción"
+                                multiline
 
-                    <Button color="primary" size="small" type="submit" startIcon={<SaveIcon />}>
-                        Guardar
-                    </Button>
-                </form>
-            </Container>
+                            />
+
+                        </div>
+
+                        <Button color="primary" size="small" type="submit" startIcon={<SaveIcon />}>
+                            Guardar
+                </Button>
+                    </form>
+                </Container>
+            </div>
+
         );
-    }
+    } else { return (<h2>Cargando...</h2>) }
+
+
+}
 }
 
 export default PostComponent;
